7240ef85e0fef10af7c29adcc342fb46005f725f
daric-e <100322530+daric-e@users.noreply.github.com>
2022-12-16 10:15:03 -0700

    finished 35.2-3

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+  
+  require("dotenv").config();
+  const { DATABASE_URL } = process.env;
+
+  // Define the database configuration object
+const config = {
+  client: "postgresql",
+  connection:
+    "postgres://myfakedatabase:8t6FiWqSDF8GsR--7mrun245I9TofnWd@fakepostgres.db.elephantsql.com:5432/myfakedatabase",
+};
+
+// Initialize a Knex instance by passing `config` as an argument to the Knex module
+const knex = require("knex")(config);
+
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+  },
+};
