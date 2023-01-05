d07f9c12268a3841eb79a42c4a6f5688911ed728
daric-e <100322530+daric-e@users.noreply.github.com>
2023-01-05 13:40:49 -0700

    35.5 knex file

--- knexfile.js
+++ knexfile.js
@@ -26,5 +26,8 @@ module.exports = {
     migrations: {
     directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+        directory: path.join(__dirname, "src", "db", "seeds"),
+      },
   },
 };
